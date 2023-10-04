baea75cb9d02cc4763377c04585e8c146aa77549
qmer05 <cph-oo203@cphbusiness.dk>
2023-10-04 15:22:33 +0200

    update

---
+++ src/task1/Customer.java
@@ -0,0 +1,37 @@
+package task1;
+
+import java.util.ArrayList;
+
+public class Customer {
+
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private int id;
+
+    public Customer(String firstName, String lastName, String userName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+    }
+
+    public String toString() {
+        return "Name: " + firstName + " " + lastName + "\tUsername: " + userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
--- src/task1/Main.java
+++ src/task1/Main.java
@@ -1,8 +1,23 @@
 package task1;
 
+import java.util.ArrayList;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        ArrayList<Customer> customers = new ArrayList();
+        Customer customer1 = new Customer("Ømer", "Øcalan", "qmer05");
+        Customer customer2 = new Customer("Lars", "Larsen", "Lassie1980");
+        customers.add(customer1);
+        customers.add(customer2);
+
+        printCustomers(customers);
+    }
+
+    public static void printCustomers(ArrayList<Customer> listOfCustomers) {
+        for (Customer i : listOfCustomers) {
+            System.out.println(i);
+        }
     }
 }
